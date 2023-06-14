a9eb49efecd9e3561ad97db120e0a9bdb03347dd
Kim-SaeByeol <toquf345@gmail.com>
2023-06-14 21:17:44 +0900

    Initial commit

---
+++ src/test/java/kopo/java_springboot_study/JavaSpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kopo.java_springboot_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
